0f45cd3c026238a655da2788d7d481939bdf3322
mukarrom21 <mukarrom21@gmail.com>
2022-04-27 09:50:31 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import CardGroup from "./components/CardGroup/CardGroup";
+import { Button, Spinner } from "react-bootstrap";
+import CardGroup2 from "./components/CardGroup2/CardGroup2";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Using React Bootstrap</h1>
+      <Button variant="success">My Button</Button>
+      <br />
+
+      <>
+        <Spinner animation="border" variant="primary" />
+        <Spinner animation="border" variant="secondary" />
+        <Spinner animation="border" variant="success" />
+        <Spinner animation="border" variant="danger" />
+        <Spinner animation="border" variant="warning" />
+        <Spinner animation="border" variant="info" />
+        <Spinner animation="border" variant="light" />
+        <Spinner animation="border" variant="dark" />
+        <Spinner animation="grow" variant="primary" />
+        <Spinner animation="grow" variant="secondary" />
+        <Spinner animation="grow" variant="success" />
+        <Spinner animation="grow" variant="danger" />
+        <Spinner animation="grow" variant="warning" />
+        <Spinner animation="grow" variant="info" />
+        <Spinner animation="grow" variant="light" />
+        <Spinner animation="grow" variant="dark" />
+      </>
+      <CardGroup2></CardGroup2>
     </div>
   );
 }
---
+++ src/components/Card/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Card = ({product}) => {
+    const {name, price}=product;
+    return (
+    <div className="card">
+        <img src="..." className="card-img-top" alt="..."/>
+        <div className="card-body">
+            <h5 className="card-title">{name}</h5>
+            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p>Price: {price}</p>
+            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+    </div>
+    );
+};
+
+export default Card;
